102aceee1f7b5e0d1086a20709a58043ef8ac458
Khaja Naquiuddin <naquiuddin@gmail.com>
2016-07-25 13:20:29 +0530

    added gitignore

---
+++ app/components/Guest.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Guest = React.createClass({
+	render: function(){
+		return (
+			<div>
+				Guest is saying hello!
+			</div>
+		);
+	}
+});
+
+module.exports = Guest;
